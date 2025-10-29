8f0946309920b83c6e138c9aa53115fd47374814
patrickmurray42-rgb <patrickmurray42@gmail.com>
2025-10-29 04:47:52 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'gray-850': '#0f1720',
+        'gunmetal': '#263238',
+        'accent-red': '#7f1d1d'
+      }
+    }
+  },
+  plugins: [],
+}
